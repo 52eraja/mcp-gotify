a5d93d71dac56db10f12560b249f15c1e4443b06
52eraja <122664422+52eraja@users.noreply.github.com>
2025-05-04 21:08:26 +0800

    Update config.py

--- src/mcp_gotify/config.py
+++ src/mcp_gotify/config.py
@@ -1,8 +1,8 @@
 from os import getenv
 from sys import stderr, exit as sys_exit
 
-GOTIFY_URL = getenv("GOTIFY_URL", "http://localhost:2081")
-GOTIFY_TOKEN = getenv("GOTIFY_TOKEN", None)
+GOTIFY_URL = getenv("GOTIFY_URL", "https://g.bvip.eu.org")
+GOTIFY_TOKEN = getenv("GOTIFY_TOKEN", An_pdhfU9X9W06p)
 
 if not GOTIFY_TOKEN:
     print("GOTIFY_TOKEN not set", file=stderr, flush=True)
